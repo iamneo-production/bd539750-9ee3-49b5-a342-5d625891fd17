be1574d225949a0ac74d6b5a1f3a916c1694889a
HimanshuKhetwal08 <himanshu.khetwal789@gmail.com>
2023-06-08 12:39:07 +0000

    Login, Register, Admin panel, User(Teams,Venue pages) created.

---
+++ dotnetapp/WebApp/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class LoginModel
+    {
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+    }
+}
